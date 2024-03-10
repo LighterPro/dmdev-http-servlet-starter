3529ce4350b72a5ab4b4bf6430878fd44005d7d4
LighterPro <LighterPro@gmail.com>
2024-03-11 00:50:29 +0300

    29. Параметры запроса. Postman

--- src/com/dmdev/http/servlet/FirstServlet.java
+++ src/com/dmdev/http/servlet/FirstServlet.java
@@ -10,13 +10,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
+import java.util.Map;
 
 /*
-* Мы уже прекрасно знаем как выглядят HTTP заголовки и зачем они нужны,
-* но теперь нам необходимо разобраться с тем, как их использовать в наших сервлетах.
-* Нам нужно знать как они получаются из HTTP запроса, а также как их устанавливать в HTTP ответах,
-* чем и будем заниматься в этом видео.
-* */
+ * Мы уже разбирали на предыдущих занятиях, что такое параметры запроса.
+ * Теперь узнаем как их передавать не одним (через URL), а двумя разными способами.
+ * Конечно же увидим, как их извлекать на стороне нашего сервлета,
+ * а также познакомимся с очень мощным и удобным HTTP клиентом - Postman.
+ */
 
 @WebServlet("/first")
 public class FirstServlet extends HttpServlet {
@@ -27,12 +28,15 @@ public class FirstServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String paramValue = req.getParameter("param");
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        System.out.println();
+
         Enumeration<String> headerNames = req.getHeaderNames();
         while (headerNames.hasMoreElements()) {
             String header = headerNames.nextElement();
             System.out.println(req.getHeader(header));
         }
-
 
         resp.setContentType("text/html");
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
@@ -41,6 +45,12 @@ public class FirstServlet extends HttpServlet {
         try (PrintWriter writer = resp.getWriter()) {
             writer.write("<h2>Hello from FirstServletZZ!!!! Привет!! </h2>");
         }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        System.out.println(parameterMap);
     }
 
     @Override
