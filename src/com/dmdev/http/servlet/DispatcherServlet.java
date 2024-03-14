b9e7a45697ea7090178468ad4ace4c5112b14482
LighterPro <LighterPro@gmail.com>
2024-03-14 19:15:58 +0300

    38. Перенаправление запросов. FORWARD

--- src/com/dmdev/http/servlet/DispatcherServlet.java
+++ src/com/dmdev/http/servlet/DispatcherServlet.java
@@ -1,6 +1,5 @@
 package com.dmdev.http.servlet;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/dispatcher")
 public class DispatcherServlet extends HttpServlet {
@@ -22,7 +20,8 @@ public class DispatcherServlet extends HttpServlet {
 //        PrintWriter writer = resp.getWriter();
 //        writer.write("Hello 2");
 
-        resp.sendRedirect("/flights");
+//        resp.sendRedirect("/flights");
+        req.getRequestDispatcher("/flights").forward(req, resp);
 
         System.out.println();
     }
